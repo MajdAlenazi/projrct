e99f503c615d79bad0b3b27af64fc8b1fc5e9ee8
Majd <majdthani@gmail.com>
2023-08-18 23:48:30 +0300

    save

--- main.js
+++ main.js
@@ -1,16 +1,31 @@
 const submit = document.getElementById("submit");
 
+let gameOptions = {
+    username: "",
+    gameMode: ""
+};
+
+const sortedGameOptions = localStorage.getItem("Game-Options");
+let gameOptionsObject = JSON.parse(sortedGameOptions);
+
+
 submit.addEventListener("click", (e) => {
     e.preventDefault();
 
     const username = document.getElementById("username").value;
 
+    gameOptionsObject.username = username;
+
+    localStorage.setItem("Game-Options", JSON.stringify(gameOptionsObject)); 
+
     if (username === "" || username === null) {
-        document.querySelector("#error-message").innerText = "Please enter your name";
+        document.querySelector("#error-message").innerText = "Enter your name";
         return false
     } else {
         document.querySelector("#error-message").innerText = ""; 
     }
+
+    
 
 });
 
